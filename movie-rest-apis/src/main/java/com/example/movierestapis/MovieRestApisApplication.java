dd02d3b567732e81719a3dda9e7cea535d148a0d
Jason Alvarado <jasonalvarado@jasons-air.attlocal.net>
2021-11-09 13:05:04 -0800

    Start of REST project

---
+++ movie-rest-apis/src/main/java/com/example/movierestapis/MovieRestApisApplication.java
@@ -0,0 +1,13 @@
+package com.example.movierestapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRestApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRestApisApplication.class, args);
+	}
+
+}
---
+++ movie-rest-apis/src/test/java/com/example/movierestapis/MovieRestApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movierestapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRestApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
