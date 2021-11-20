1a0f16e5951eeaf660ac536af9bd8a21328d2ef9
Jason Alvarado <jasonalvarado@jasons-air.attlocal.net>
2021-11-19 19:24:16 -0800

    Returns json collection of titles

--- moviepractice/src/main/java/com/example/moviepractice/Controller/MovieRestController.java
+++ moviepractice/src/main/java/com/example/moviepractice/Controller/MovieRestController.java
@@ -1,8 +1,13 @@
 package com.example.moviepractice.Controller;
 import com.example.moviepractice.Model.Movie;
+import com.fasterxml.jackson.annotation.JsonFilter;
+import com.fasterxml.jackson.core.JsonParseException;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 //import OMDB file for use below
@@ -12,6 +17,9 @@ import java.util.concurrent.atomic.AtomicLong;
 import OmdbApi.OmdbController;
 import OmdbApi.OmdbWebServiceClient;
 import OmdbApi.OmdbController;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 //want to be json so use RestController
@@ -21,14 +29,38 @@ public class MovieRestController {
     private static final String template = "Hello, %s";
     private final AtomicLong counter = new AtomicLong();
 
+    //filter json function
+    // public String filterJson(String response) throws JsonParseException {
+    //     List<String> filtered = new ArrayList<>();
+    //     JSONARRAy
+    // } 
+
     //only returns the title
     //http://localhost:8080/movies?title="INSERT TITLE HERE"
-    @GetMapping("/movies")
+    @GetMapping("/award")
     Movie title(@RequestParam(defaultValue = "null") String title) {
-        String inputTitle = "whiplash";
+        String inputTitle = "{parasite}";
         String response = OmdbWebServiceClient.searchMovieByTitle(inputTitle, "fe57eabc");
         OmdbController test = new OmdbController(response);
+
+
         return new Movie(counter.incrementAndGet(), test.getJsonResponse(), String.format(template, title), 0);
         // return new Movie(counter.incrementAndGet(), String.format(template, title), test.getJsonResponse(), 0);
     }
+    
+    @RequestMapping("/movies/{title}")
+    public @ResponseBody String getResponse(@PathVariable(value="title") String titles) {
+        String response = OmdbWebServiceClient.searchMovieByTitle(titles, "fe57eabc");
+        OmdbController test = new OmdbController(response);
+        return test.getJsonResponse();
+    }
+
+    // String response = OmdbWebServiceClient.SEARCH_BY_IMDB_URL;
+
+    // @RequestMapping("/movies/{title}")
+    // public @ResponseBody String getResponse(@PathVariable(value="title") String titles) {
+    //     String response = OmdbWebServiceClient.SEARCH_BY_IMDB_URL;
+    //     OmdbController test = new OmdbController(response);
+    //     return test.getJsonResponse();
+    // }
 }
